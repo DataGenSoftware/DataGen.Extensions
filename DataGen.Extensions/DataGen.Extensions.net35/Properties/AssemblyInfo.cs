f8cfa091442688feffb810377d00255fbbb2140e
karolhabczyk <karol.habczyk@gmail.com>
2015-05-23 17:59:07 +0200

    Remove assembly GUID

--- DataGen.Extensions/DataGen.Extensions.net35/Properties/AssemblyInfo.cs
+++ DataGen.Extensions/DataGen.Extensions.net35/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e935a8b-1c61-4945-b739-26fa2cd44088")]
+//[assembly: Guid("2e935a8b-1c61-4945-b739-26fa2cd44088")]
--- DataGen.Extensions/DataGen.Extensions.net45/Properties/AssemblyInfo.cs
+++ DataGen.Extensions/DataGen.Extensions.net45/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6abf3777-3044-4781-8770-8912a6de96ce")]
+//[assembly: Guid("6abf3777-3044-4781-8770-8912a6de96ce")]
