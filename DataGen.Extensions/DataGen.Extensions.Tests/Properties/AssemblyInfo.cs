abb37c747ffad8ddd3eb17ed48d6e90b13595e95
Karol Habczyk <karol.habczyk@gmail.com>
2016-01-14 22:23:41 +0100

    Reorganize project

--- DataGen.Extensions/DataGen.Extensions.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("b18ed368-6143-41d1-96f3-f64246401fba")]
--- DataGen.Extensions/DataGen.Extensions.net40/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("59fe7846-267e-4abc-8017-67fb2c7c1a3f")]
