b68b7a0b836ca135a852a3dcdcefcaba1d60f8c9
Karol Habczyk <karol.habczyk@gmail.com>
2016-01-14 22:46:20 +0100

    Publish project

--- DataGen.Extensions/DataGen.Extensions.Publish/Program.cs
+++ DataGen.Extensions/DataGen.Extensions.Publish/Program.cs
@@ -10,6 +10,43 @@ namespace DataGen.Extensions.Publish
     {
         static void Main(string[] args)
         {
+            DisplayMenu();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("1 - Rebuild");
+            Console.WriteLine("2 - Change verion");
+            Console.WriteLine("3 - Publish to NuGet");
+            Console.WriteLine("0 - Exit");
+
+            Console.ReadKey();
+        }
+
+        private static void HandleCommand(string command)
+        {
+
+        }
+
+        private static void Rebuild()
+        {
+
+        }
+
+        private static void ChangeVersion()
+        {
+
+        }
+
+        private static void PublishToNuget()
+        {
+
+        }
+
+        private static void Exit()
+        {
+            
         }
     }
+
 }
