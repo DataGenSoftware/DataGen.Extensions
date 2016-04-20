ebbf3d6ee77558fb93553719299fa62df232de5a
Karol Habczyk <karol.habczyk@gmail.com>
2016-04-20 21:54:24 +0200

    Fix DataGen.RomanNumerals  project

--- DataGen.Extensions/DataGen.RomanNumerals/Properties/AssemblyInfo.cs
+++ DataGen.Extensions/DataGen.RomanNumerals/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataGen.RomanNumerals")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Roman numerals extensions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DataGen.RomanNumerals")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Karol Habczyk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad50d653-3c28-42fd-b564-220599f9662a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +29,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
