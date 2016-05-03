3e43d98c62c2f87d5c7bf6347d02354198bbb068
Karol Habczyk <karol.habczyk@gmail.com>
2016-05-03 19:00:12 +0200

    #11 & #12 Add Next & Previous collection extension

---
+++ DataGen.Extensions/DataGen.Extensions/CollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGen.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static T Previous<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            CheckIfCollectionArgumentIsNull(collection);
+            CheckIfPredicateArgumentIsNull(predicate);
+
+            return collection.TakeWhile(x => !predicate(x)).Last();
+        }
+
+        public static T Next<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            CheckIfCollectionArgumentIsNull(collection);
+            CheckIfPredicateArgumentIsNull(predicate);
+
+            return collection.SkipWhile(x => !predicate(x)).Skip(1).First();
+        }
+
+        private static void CheckIfPredicateArgumentIsNull<T>(Func<T, bool> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+        }
+
+        private static void CheckIfCollectionArgumentIsNull<T>(IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection");
+            }
+        }
+    }
+}
---
+++ DataGen.Extensions/DataGen.Extentions.UnitTests/CollectionExtensionsTests.cs
@@ -0,0 +1,123 @@
+﻿using DataGen.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGen.Extentions.UnitTests
+{
+    [TestFixture]
+    public class CollectionExtensionsTests
+    {
+        [Test]
+        public void IEnumerableNext_Element_ReturnsNextElementFromEnumerator()
+        {
+            var collection = this.MakeIntCollection();
+
+            var actual = collection.Next(x => x == 16);
+
+            var expected = 8;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IEnumerableNext_ElementDoesntExists_ThrowsInvalidOperationException()
+        {
+            var collection = this.MakeStringCollection();
+ 
+            Assert.Throws<InvalidOperationException>(() => collection.Next(x => x == "twenty"));
+        }
+
+        [Test]
+        public void IEnumerableNext_LastElement_ThrowsInvalidOperationException()
+        {
+            var collection = this.MakeStringCollection();
+
+            Assert.Throws<InvalidOperationException>(() => collection.Next(x => x == "ten"));
+        }
+
+        [Test]
+        public void IEnumerableNext_NullCollection_ThrowsArgumentNullException()
+        {
+            IEnumerable<string> collection = null;
+
+            Assert.Throws<ArgumentNullException>(() => collection.Next(x => x == "twenty"));
+        }
+
+        [Test]
+        public void IEnumerableNext_NullPredicate_ThrowsArgumentNullException()
+        {
+            var collection = this.MakeDateTimeCollection();
+
+            Assert.Throws<ArgumentNullException>(() => collection.Next(null));
+        }
+
+        [Test]
+        public void IEnumerablePrevious_Element_ReturnsPreviousElementFromEnumerator()
+        {
+            var collection = this.MakeDateTimeCollection();
+
+            var actual = collection.Previous(x => x == new DateTime(2016, 3, 11));
+
+            var expected = new DateTime(2016, 4, 14);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IEnumerablePrevious_ElementDoesntExists_ThrowsInvalidOperationException()
+        {
+            var collection = this.MakeIntCollection();
+
+            Assert.Throws<InvalidOperationException>(() => collection.Next(x => x == 7));
+        }
+
+        [Test]
+        public void IEnumerablePrevious_FirstElement_ThrowsInvalidOperationException()
+        {
+            var collection = this.MakeIntCollection();
+
+            Assert.Throws<InvalidOperationException>(() => collection.Previous(x => x == 1));
+        }
+
+        [Test]
+        public void IEnumerablePrevious_NullCollection_ThrowsArgumentNullException()
+        {
+            IEnumerable<int> collection = null;
+
+            Assert.Throws<ArgumentNullException>(() => collection.Previous(x => x == 10));
+        }
+
+        [Test]
+        public void IEnumerablePrevious_NullPredicate_ThrowsArgumentNullException()
+        {
+            var collection = this.MakeDateTimeCollection();
+
+            Assert.Throws<ArgumentNullException>(() => collection.Previous(null));
+        }
+
+
+        private IEnumerable<int> MakeIntCollection()
+        {
+            return new List<int>() { 1, 3, 4, 16, 8, 19, 10, 11, 2, 15, 9 };
+        }
+
+        private IEnumerable<string> MakeStringCollection()
+        {
+            return new List<string>() { "one", "three", "four", "six", "eight", "nine" , "ten" };
+        }
+
+        private IEnumerable<DateTime> MakeDateTimeCollection()
+        {
+            return new List<DateTime>()
+            {
+                new DateTime(2016, 1, 1),
+                new DateTime(2016, 4, 14),
+                new DateTime(2016, 3, 11),
+                new DateTime(2016, 5, 19),
+                new DateTime(2016, 6, 23),
+            };
+        }
+    }
+}
