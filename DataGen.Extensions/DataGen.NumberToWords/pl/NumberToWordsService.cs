94b46d8e2c6b5d6d8b1398c29f45cf29e4acedfd
Karol Habczyk <karol.habczyk@gmail.com>
2016-05-23 21:21:33 +0200

    NumberToWords refactor missing files

--- DataGen.Extensions/DataGen.NumberToWords/pl/NumberToWordsService.cs
+++ DataGen.Extensions/DataGen.NumberToWords/pl/NumberToWordsService.cs
@@ -8,68 +8,12 @@ namespace DataGen.NumberToWords.Pl
 {
     public class NumberToWordsService: Common.NumberToWordsService
     {
-        private Common.NumeralsRepository NumeralsRepository { get; set; }
-
         public NumberToWordsService(Common.NumeralsRepository numeralsRepository)
+            :base (numeralsRepository)
         {
-            this.NumeralsRepository = numeralsRepository;
         }
 
-        public override string InWords(int value)
-        {
-            if (value < 0 || value > 999999999)
-            {
-                throw new ArgumentOutOfRangeException("value");
-            }
-
-            var result = string.Empty;
-
-            result = ProcessPartOfValue(value, result, NumeralOrderOfMagnitude.Milion);
-
-            result = ProcessPartOfValue(value, result, NumeralOrderOfMagnitude.Thousand);
-
-            result = ProcessPartOfValue(value, result, NumeralOrderOfMagnitude.Unit);
-
-            return result.Trim();
-        }
-
-        private string ProcessPartOfValue(int value, string result, NumeralOrderOfMagnitude numeralOrderOfMagnitude)
-        {
-            int partOfValue = this.GetPartOfValue(value, numeralOrderOfMagnitude);
-
-            if (partOfValue > 0)
-            {
-                string extension = this.GetNumeralExtension(partOfValue, numeralOrderOfMagnitude);
-
-                while (partOfValue > 0)
-                {
-                    int operatorValue = this.NumeralsRepository.Numerals.OrderByDescending(x => x.Key).First(x => x.Key <= partOfValue).Key;
-                    result += " " + this.NumeralsRepository.GetNumeral(operatorValue);
-                    partOfValue -= operatorValue;
-                }
-
-                result += " " + extension;
-            }
-
-            return result;
-        }
-
-        private int GetPartOfValue(int value, NumeralOrderOfMagnitude numeralOrderOfMagnitude)
-        {
-            switch (numeralOrderOfMagnitude)
-            {
-                case NumeralOrderOfMagnitude.Unit:
-                    return this.GetUnits(value);
-                case NumeralOrderOfMagnitude.Thousand:
-                    return this.GetThousands(value);
-                case NumeralOrderOfMagnitude.Milion:
-                    return this.GetMilions(value);
-                default:
-                    return 0;
-            }
-        }
-
-        private string GetNumeralExtension(int value, NumeralOrderOfMagnitude orderOfMagnitude)
+        protected override string GetNumeralExtension(int value, NumeralOrderOfMagnitude orderOfMagnitude)
         {
             NumeralGrammaticalCase grammaticalCase = NumeralGrammaticalCase.SingularNominative;
             
@@ -89,21 +33,6 @@ namespace DataGen.NumberToWords.Pl
                 return this.NumeralsRepository.NumeralExtensions[numeralExtensionsKey];
             }
             return string.Empty;
-        }
-
-        private int GetUnits(int value)
-        {
-            return value % 1000;
-        }
-
-        private int GetThousands(int value)
-        {
-            return Convert.ToInt32(Math.Floor(Convert.ToDecimal((value % 1000000) / 1000)));
-        }
-
-        private int GetMilions(int value)
-        {
-            return Convert.ToInt32(Math.Floor(Convert.ToDecimal((value % 1000000000) / 1000000)));
         }
     }
 }
