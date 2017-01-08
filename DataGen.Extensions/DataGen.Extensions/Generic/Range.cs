806c2f9e80331ef2494c8e2ba6f5707f71197349
Karol <karol.habczyk@gmail.com>
2017-01-08 23:14:57 +0100

    #34 Add Range generic class

---
+++ DataGen.Extensions/DataGen.Extensions/Generic/Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGen.Extensions.Generic
+{
+    public class Range<T>
+    {
+        public T Start { get; set; }
+
+        public T End { get; set; }
+
+        public Range(T start, T end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
